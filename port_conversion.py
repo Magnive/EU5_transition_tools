bf5e3c4ba7a68111d01854435bcb0b2f3a6f8e4a
PW-Hansen <Peterweishansen@Hotmail.com>
2025-12-01 17:04:01 +0100

    eu4 -> eu5 port conversions

---
+++ port_conversion.py
@@ -0,0 +1,35 @@
+import csv
+
+top_margin = 731
+bottom_margin = 1503
+
+old_height = 2048
+old_width = 5632
+
+new_height = 8192
+new_width = 16384
+actual_height = new_height - top_margin - bottom_margin
+
+
+def convert_coords(x, y):
+    new_x = int((x / old_width) * new_width)
+    new_y = int((y / old_height) * actual_height) + bottom_margin
+    return new_x, new_y
+
+
+with open('input\\eu4_ports.csv', 'r', encoding='utf-8') as infile, \
+    open('output\\game\\in_game\\map_data\\eu5_ports.csv', 'w', encoding='utf-8', newline='') as outfile:
+    reader = csv.DictReader(infile, delimiter=';')
+    fieldnames = reader.fieldnames
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter=';')
+    writer.writeheader()
+    
+    for row in reader:
+        x = int(row.get('x'))
+        y = int(row.get('y'))
+        new_x, new_y = convert_coords(x, y)
+        row['x'] = new_x
+        row['y'] = new_y
+        writer.writerow(row)
+
+
